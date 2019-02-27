a29f1597a30905ea6221f458c7f020fc000ba08f
Tristan Anderson <ande0498@algonquinlive.com>
2019-02-27 09:46:13 -0500

    Moving files into seperate directories for WebVR and Mobile

--- app.js
+++ app.js
@@ -4,18 +4,19 @@ const http      = require('http');
 const server    = http.createServer(app);
 
 //convenience vars
-const LISTEN_PORT = 8080;
+const PORT_WEBVR = 8080;
+const PORT_MOBILE = 8000;
 
-//middleware This set out base html to emerge from the publicm folder
-app.use(express.static(__dirname + '/public'));
+// -- WebVR -- //
+//middleware This set out base html to emerge from the public folder
+app.use(express.static(__dirname + '/public/WebVR'));
 
 //set a route
 app.get('/', function(req, res){
-    res.sendFile(__dirname + 'public/index.html');
+    res.sendFile(__dirname + 'public/WebVR/index.html');
 });
 
-
 //finally, start server
-server.listen(LISTEN_PORT);
+server.listen(PORT_WEBVR);
 
 console.log("Listening on port: " + LISTEN_PORT); //print statement to console/terminal
