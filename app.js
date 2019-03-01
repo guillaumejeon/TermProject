aef2e420dee44cbdd9b5559b618a6877a8f911f0
Tristan Anderson <Hiyate@users.noreply.github.com>
2019-03-01 13:40:27 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,19 +4,18 @@ const http      = require('http');
 const server    = http.createServer(app);
 
 //convenience vars
-const PORT_WEBVR = 8080;
-const PORT_MOBILE = 8000;
+const LISTEN_PORT = 8080;
 
 // -- WebVR -- //
 //middleware This set out base html to emerge from the public folder
-app.use(express.static(__dirname + '/public/WebVR'));
+app.use(express.static(__dirname + '/public'));
 
 //set a route
 app.get('/', function(req, res){
-    res.sendFile(__dirname + 'public/WebVR/index.html');
+    res.sendFile(__dirname + 'public/index.html');
 });
 
 //finally, start server
-server.listen(PORT_WEBVR);
+server.listen(LISTEN_PORT);
 
 console.log("Listening on port: " + LISTEN_PORT); //print statement to console/terminal
