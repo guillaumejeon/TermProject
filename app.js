609b15381eebf1f9617bf966c8fbc154271e8b65
Tristan Anderson <ande0498@algonquinlive.com>
2019-03-01 15:56:14 -0500

    Updated app.js to connect to MongoDB database (currently NOT working)

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
-const express   = require('express');
-const app       = express();
-const http      = require('http');
-const server    = http.createServer(app);
+const express       = require('express');
+const app           = express();
+const http          = require('http');
+const server        = http.createServer(app);
+/*
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb://kay:yo-yo123@SandboxExperiences-shard-00-00.mongodb.net:27017,SandboxExperiences-shard-00-01.mongodb.net:27017,SandboxExperiences-shard-00-02.mongodb.net:27017/admin?ssl=true&replicaSet=SandboxExperiences-shard-0";
+*/
 
 //convenience vars
-const LISTEN_PORT = 8080;
+const LISTEN_PORT   = 8080;
 
-// -- WebVR -- //
+/*
+// Set up connection to database
+MongoClient.connect(uri, function(err, client) {
+   const collection = client.db("test").collection("devices");
+   // perform actions on the collection object
+   client.close();
+});
+*/
+
 //middleware This set out base html to emerge from the public folder
 app.use(express.static(__dirname + '/public'));
 
 //set a route
 app.get('/', function(req, res){
-    res.sendFile(__dirname + 'public/app.html');
+    res.sendFile(__dirname + 'public/index.html');
 });
 
 //finally, start server
