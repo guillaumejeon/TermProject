b9196483129b11a3cba3077a453873e4bed2c729
Tristan Anderson <ande0498@algonquinlive.com>
2019-03-01 14:07:49 -0500

    PHP test

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.static(__dirname + '/public'));
 
 //set a route
 app.get('/', function(req, res){
-    res.sendFile(__dirname + 'public/index.html');
+    res.sendFile(__dirname + 'public/app.html');
 });
 
 //finally, start server
