aff0d42543c3bd4c95276b77c07d8ccf447a7462
guillaumejeon <46755079+guillaumejeon@users.noreply.github.com>
2019-03-15 02:55:12 -0400

    lighting

--- public/js/Menu.js
+++ public/js/Menu.js
@@ -6,14 +6,16 @@ AFRAME.registerComponent('menutoggle', {
             
             var el = this.el
 
+            el.querySelector = document.querySelector("#skipBackButton-obj");
+
             // var skipButton = document.querySelector("#skipBackButton-obj");
 
             // var toggle = true;
 
             el.addEventListener('click', function (e){
                 console.log("Hello");
-                let skipButton = document.querySelector("#skipBackButton-obj");
-                skipButton.setAttribute('visible', false);
+                // let skipButton = document.querySelector("#skipBackButton-obj");
+                // setAttribute('visible', false);
                 // toggle = false;
             })
         }
