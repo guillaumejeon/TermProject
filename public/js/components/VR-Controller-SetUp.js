1df6c271c2efb0534ed6bd3b224fabaa831b71f2
DeltaChrome <98hawkeye@gmail.com>
2019-04-07 20:46:44 -0400

    fixing controller

--- public/js/components/VR-Controller-SetUp.js
+++ public/js/components/VR-Controller-SetUp.js
@@ -1,10 +1,23 @@
 function ControllerSetup(hand, colour)
 {
-    var controller = document.getElementById('controller');
+    let controller = document.createElement('a-entity');
     controller.setAttribute('laser-controls', {hand: hand});
-    controller.setAttribute('line', {color: colour});
+    controller.setAttribute('raycaster', 'objects: .link; far: 10')
+    controller.setAttribute('line', 'color: ' + colour + '; opacity: 0.75');
+    
+    let orb = document.createElement('a-entity');
 
-    var orb = document.getElementById('orb');
-    orb.setAttribute('material', {color: colour});
-    orb.setAttribute('light', {color: colour});
+    if (hand == 'right')
+        orb.setAttribute('position', {x: -0.0075, y: -0.04, z: -0.04});
+    else if (hand == 'left')
+        orb.setAttribute('position', {x: 0.0075, y: -0.04, z: -0.04});
+
+    orb.setAttribute('geometry', 'primitive: sphere; radius: 0.025; segments-height: 16; segments-width: 32')
+    orb.setAttribute('material', 'color: ' + colour + '; opacity: 0.75; shader: flat');
+    orb.setAttribute('light', 'type: point; color: ' + colour + '; intensity: 0.5; distance: 1; decay: 4');
+
+    controller.appendChild(orb);
+
+    let scene = document.querySelector('a-scene');
+    scene.appendChild(controller);
 }
