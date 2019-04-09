b24b77d7ba4518daf588618cc1ce74211d22cf91
Tristan Anderson <ande0498@algonquinlive.com>
2019-04-08 21:46:03 -0400

    finalizing settings and implementing tutorial

--- public/js/Setup.js
+++ public/js/Setup.js
@@ -22,7 +22,52 @@ function ControllerSetup(hand, colour)
     scene.appendChild(controller);
 }
 
-function BuddySetup(use)
+function BuddySetup(use, tutorial)
 {
-    document.getElementById('breath-buddy').setAttribute('visible', use);
+    if (use)
+    {
+        var buddy = document.getElementById('breath-buddy')
+        var aura = document.getElementById('breath-buddy-case');
+        
+        var anim = 
+        {
+            property: 'position',
+            dur: '7500',
+            delay: '0',
+            easing: 'easeInQuad',
+            from: '-10 1 -3',
+            to: '0 1 -3'
+        }
+        console.log(anim);
+
+        buddy.setAttribute('animation__setup', {property: anim.property, dur: anim.dur, delay: anim.delay, easing: anim.easing, from: anim.from, to: anim.to});
+        setTimeout(function()
+        {
+            buddy.removeAttribute('animation__setup');
+        }, 7500);
+
+        if (tutorial)
+        {
+            anim = 
+            {
+                property: 'geometry.radius',
+                dur: '1000',
+                delay: '7500',
+                dir: 'alternate',
+                easing: 'easeInQuad',
+                from: '0.25',
+                to: '1'
+            }
+            console.log(anim);
+
+            aura.setAttribute('animation__test', 'property:' + anim.property + ';  dur:' + anim.dur + '; delay:' + anim.delay + '; dir:' + anim.dir +'; easing:' + anim.easing + '; from:' + anim.from + '; to:' + anim.to);
+
+            setTimeout(function()
+            {
+                aura.removeAttribute('animation__test');
+            }, 9500);
+        }
+    }
+    else
+        document.getElementById('breath-buddy').setAttribute('visible', false);
 }
